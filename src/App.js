3c8d8d03ed4a2ff07303a478b71c11e5057c35cc
Emre Kutan Nural <emrenural.elk@gmail.com>
2023-09-14 02:53:35 +0300

    ilk react push

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+function App() {
+  return (
+    <div className="App">
+      <img src="logo192.png" alt="React Logo" /> 
+      <img src={logo} alt="" />
+    </div>
+  );
+}
+
+export default App;
